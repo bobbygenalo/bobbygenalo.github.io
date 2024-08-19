faef8e661e083fc084ba689988698daba92c30e9
Bobby Genalo <bobbygenalo@users.noreply.github.com>
2024-08-19 14:16:41 -0700

    First commit
    Pushing very old code, fingers crossed!

---
+++ :msk/msk.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  // $('.menu a').on('click', function(e) {
+  //
+  //
+  //
+  //   $(this).toggleClass("active");
+  //
+  //
+  // });
+
+
+  // var shiftText = $("#shiftText");
+  //
+  // shiftText.on("scroll", function(e) {
+  //
+  // if (this.scrollTop > 150) {
+  //   shiftText.addClass("fix-copy");
+  //   console.log('got it');
+  // } else {
+  //   shiftText.removeClass("fix-copy");
+  // }
+  //
+  // });
+
+});
---
+++ genalo.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.home').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 6000,
+    initialSlide: 0,
+    mobileFirst: true,
+  });
+
+});
